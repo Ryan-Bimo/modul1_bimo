c8fd7705658c25d893b79ac612dffb65b553feee
Ryan-Bimo <ytbimo15@gmail.com>
2026-07-21 10:46:54 +0700

    update lkpd

--- lkpd1 bagian2/kegiatan2tahap2.js
+++ lkpd1 bagian2/kegiatan2tahap2.js
@@ -1,30 +1,26 @@
 class Rekening {
     #saldo;
- 
     constructor() {
-        this.#saldo = 5000000;
+        this.#saldo = 100000;
     }
- 
     setor(jumlah) {
         // TODO: tambahkan jumlah ke this.#saldo
         this.#saldo += jumlah;
     }
- 
     tarik(jumlah) {
         // TODO: kurangi this.#saldo jika jumlah <= this.#saldo,
         // jika tidak tampilkan pesan "Saldo tidak cukup"
         if (jumlah <= this.#saldo) {
             this.#saldo -= jumlah;
         } else {
-            console.log("saldo tidak cukup");
+            document.write("Saldo tidak cukup");
         }
     }
- 
-    getSaldo() {
+    getSaldo() { 
         return this.#saldo;
     }
 }
-const Rekening = new Rekening();
-Rekening.setor(1000);
-Rekening.tarik(500);
-console.log(rekening.getSaldo());
+const rekening = new Rekening();
+rekening.setor(5000);
+rekening.tarik(2000);
+document.write(rekening.getSaldo()); 
